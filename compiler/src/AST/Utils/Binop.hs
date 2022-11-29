2d4ab1eb548c8cdffc846e783a8eaffb9bc22b8f
Shojin Masuda <shomasd@gmail.com>
2022-11-30 00:18:40 +0900

    Show canonical

--- compiler/src/AST/Utils/Binop.hs
+++ compiler/src/AST/Utils/Binop.hs
@@ -1,34 +1,27 @@
 {-# OPTIONS_GHC -Wall #-}
+
 module AST.Utils.Binop
-  ( Precedence(..)
-  , Associativity(..)
+  ( Precedence (..),
+    Associativity (..),
   )
-  where
+where
 
-
-import Prelude hiding (Either(..))
 import Control.Monad (liftM)
 import Data.Binary
-
-
+import Prelude hiding (Either (..))
 
 -- BINOP STUFF
 
-
 newtype Precedence = Precedence Int
-  deriving (Eq, Ord)
-
+  deriving (Eq, Ord, Show)
 
 data Associativity
   = Left
   | Non
   | Right
-  deriving (Eq)
-
-
+  deriving (Eq, Show)
 
 -- BINARY
-
 
 instance Binary Precedence where
   get =
@@ -37,19 +30,19 @@ instance Binary Precedence where
   put (Precedence n) =
     put n
 
-
 instance Binary Associativity where
   get =
-    do  n <- getWord8
-        case n of
-          0 -> return Left
-          1 -> return Non
-          2 -> return Right
-          _ -> fail "Error reading valid associativity from serialized string"
+    do
+      n <- getWord8
+      case n of
+        0 -> return Left
+        1 -> return Non
+        2 -> return Right
+        _ -> fail "Error reading valid associativity from serialized string"
 
   put assoc =
     putWord8 $
       case assoc of
-        Left  -> 0
-        Non   -> 1
+        Left -> 0
+        Non -> 1
         Right -> 2
--- compiler/src/Data/Name.hs
+++ compiler/src/Data/Name.hs
@@ -66,8 +66,6 @@ import qualified Elm.String as ES
 type Name =
   Utf8.Utf8 ELM_NAME
 
-instance Show Name where
-  show = toChars
 
 data ELM_NAME
 
